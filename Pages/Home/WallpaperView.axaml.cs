3462a6ea7c45f498f41f9a47e996a14c6242e163
emptychan <chen19941018@live.com>
2024-05-28 00:28:32 +0800

    <feat>add hover

--- Pages/Home/WallpaperView.axaml.cs
+++ Pages/Home/WallpaperView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Pap.erNet.Pages.Home;
@@ -9,6 +10,18 @@ public partial class WallpaperView : UserControl
     public WallpaperView()
     {
         InitializeComponent();
+    }
+
+    private void GotEnter(object? sender, PointerEventArgs e)
+    {
+        Author.IsVisible = true;
+        ResolutionRatio.IsVisible = true;
+    }
+
+    private void GotExit(object? sender, PointerEventArgs e)
+    {
+        Author.IsVisible = false;
+        ResolutionRatio.IsVisible = false;
     }
 }
 
--- Services/WallpaperListService.cs
+++ Services/WallpaperListService.cs
@@ -12,19 +12,19 @@ public class WallpaperListService
         new Wallpaper()
         {
             Url = "https://images.unsplash.com/photo-1716619222059-62e8670293e6?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            Author = "@ZHENYU LUO",
+            Author = "@ZHENYU LUO / Unsplash",
             ResolutionRatio = "4K"
         },
         new Wallpaper()
         {
             Url = "https://images.unsplash.com/photo-1716369967339-e40e46c00c05?q=80&w=2304&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            Author = "@Victória Duarte",
+            Author = "@Victória Duarte / Unsplas",
             ResolutionRatio = "5K"
         },
         new Wallpaper()
         {
             Url = "https://images.unsplash.com/photo-1715521801502-42db7b79573b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMDZ8fHxlbnwwfHx8fHw%3D",
-            Author = "@Ahmed",
+            Author = "@Ahmed / Unsplas",
             ResolutionRatio = "2K"
         }
     };
